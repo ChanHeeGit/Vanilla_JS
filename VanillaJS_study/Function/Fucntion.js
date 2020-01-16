575be3397b1c772586f5f231bf249f27a7bd278b
ChanHee <l01083582454@gmail.com>
2020-01-16 13:34:49 +0900

    function 사용법 object와의 구분

---
+++ VanillaJS_study/Function/Fucntion.js
@@ -0,0 +1,24 @@
+
+//인자를 받아서 log..
+
+// function sayHello(name, age){
+//    return 
+//    `hello ${name} you are ${age} years old`; //백틱 (`) 사용
+// }
+
+// const greet = sayHello("ChanHee", 14);
+
+// console.log(greet);
+
+const calculator = {
+    plus: function(a, b){
+        return a+b;
+    }
+}
+
+
+const plus = calculator.plus(5,5);
+console.log(plus);
+
+//console, calculator는 객체다
+
