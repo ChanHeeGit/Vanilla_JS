c6b421595c8a145ea7e5845a129cf70c59879e71
ChanHee <l01083582454@gmail.com>
2020-01-21 11:02:46 +0900

    ues setInterval method

--- VanillaJS_App/clock.js
+++ VanillaJS_App/clock.js
@@ -9,13 +9,16 @@ function getTime(){
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
-    
+    clockTitle.innerText = `${
+        hours < 10 ? `0${hours}` : hours}:${
+        minutes < 10 ? `0${minutes}` : minutes}:${
+        seconds < 10 ? `0${seconds}` : seconds
+    }`;
 }
 function init(){
     getTime();
-
-    
+    //setInterVal : 첫번째인자 : 실행할function, 두번째인자 : 시간간격
+    setInterval(getTime, 1000);
 }
 
 init();
