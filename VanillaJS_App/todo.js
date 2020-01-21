cf6b5cbd369956a8bc0b6e911a104c3b01a3a0fc
ChanHee <l01083582454@gmail.com>
2020-01-21 16:23:54 +0900

    toDoList에 저장 // object <-> string 형변환 // 화면에 출력

--- VanillaJS_App/todo.js
+++ VanillaJS_App/todo.js
@@ -3,18 +3,33 @@ const toDoForm = document.querySelector(".js-toDoForm")
     ,toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
+const toDos = [];
 
+function saveToDos(){
+    //localStorage에는 js의 object를 저장할 수 없고 string만 저장할 수 있음
+    //stringify : object -> string 변경
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+   
 function paintToDO(text){
     //create li tag
     const li = document.createElement("li");
-    //create button tag
     const delBtn = document.createElement("button");
-    delBtn.innerHTML = "X";
     const span = document.createElement("span");
+    const newId = toDos.length + 1; // id값의 초기값 : 0 + 1 = 1
+    delBtn.innerHTML = "X";
     span.innerText = text;
     li.appendChild(delBtn); // delBtn을 li에 넣음
     li.appendChild(span); //span을 li안에 넣고
+    li.id = newId; // li에 id값을 부여
     toDoList.appendChild(li); // 최종적으로 toDoList에 li를 넣음
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    //array에 push
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event){
@@ -25,9 +40,13 @@ function handleSubmit(event){
 }
 
 function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null){
-
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    //string으로 저장된 data를 불러와야 하니 다시 object로 변환해준다
+    if(loadedToDos !== null){
+        const parseToDos = JSON.parse(loadedToDos);
+        parseToDos.forEach(function(toDo){
+           paintToDO(toDo.text);
+        });
     }
 }
 
